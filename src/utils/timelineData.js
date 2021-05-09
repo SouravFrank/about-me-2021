adc85d57804a952f2de08483492f6004df8ca01d
Satyajit Choudhary <satyajitchoudhary09@gmail.com>
2021-05-09 18:10:58 +0530

    Tree View

---
+++ src/utils/timelineData.js
@@ -0,0 +1,32 @@
+export const timelineData = [
+    {
+      "title": "Title 1",
+      "description": "Desc 1",
+      "date": "1 March 1990",
+    },
+    {
+      "title": "Title 2",
+      "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "date": "2 March 1994",
+    },
+    {
+      "title": "Title 3",
+      "description": "Desc 3",
+      "date": "2 March 1995",
+    },
+    {
+      "title": "Title 4",
+      "description": "Desc 4",
+      "date": "2 March 1996",
+    },
+    {
+      "title": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+      "description": "Desc 5",
+      "date": "2 March 1997",
+    },
+    {
+      "title": "Title 6",
+      "description": "Desc 6",
+      "date": "2 March 1998",
+    }
+  ] ;
