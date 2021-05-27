73dcc7e2a2046a553dacf90d30d1daf39070686b
Sourav2282 <ssadhukhan990@gmail.com>
2021-05-28 02:28:36 +0530

    nav and cv animated now

--- src/components/background.js
+++ src/components/background.js
@@ -1,29 +1,25 @@
 import React from "react";
+import "../styles/common.css";
 
 export const HeaderBg = () => {
   return (
     <div
-      style={{
-        height: "0px",
-        width: "0px",
-        borderRight: "85vw solid white",
-        borderTop: "100vh solid #aaa0ff",
-      }}
+      className="headerBg"
     ></div>
   );
 };
 
 export const FooterBg = () => {
-    return (
-      <div
-        style={{
-          height: "0px",
-          width: "0px",
-          float: 'right',
-          borderLeft: "75vw solid white",
-          borderBottom: "80vh solid #aaa0ff",
-        }}
-      ></div>
-    );
-  };
-  
+  return (
+    <div
+      className="footerBg"
+      // style={{
+      //   height: "0px",
+      //   width: "0px",
+      //   float: "right",
+      //   borderLeft: "75vw solid white",
+      //   borderBottom: "80vh solid #aaa0ff",
+      // }}
+    ></div>
+  );
+};
--- src/components/downloadCV.js
+++ src/components/downloadCV.js
@@ -1,21 +1,57 @@
-import React from "react";
-import AttachFileIcon from "@material-ui/icons/AttachFile";
+import React, { useState } from "react";
+// import AttachFileIcon from "@material-ui/icons/AttachFile";
 import Typewriter from "./common/typewritter";
 import { downloadResumeConfig } from "../utils/config";
 import "../styles/downloadCV.css";
 
-const DownloadResume = () => {
-  const { fileName, fileLink } = downloadResumeConfig;
+const Download = ({ checked, setchecked, downloadResumeConfig }) => {
+  const { fileLink } = downloadResumeConfig;
+
+  const downloadFile = () => {
+    if (!checked) {
+      window.location = fileLink;
+    }
+    setchecked(!checked);
+  };
 
   return (
-    <div id="resume" style={{ margin: "100px" }}>
+    <div id="ms-container">
+      <label for="ms-download">
+        <div class="ms-content">
+          <div class="ms-content-inside">
+            <input
+              onChange={downloadFile}
+              value={checked}
+              type="checkbox"
+              id="ms-download"
+            />
+            <div class="ms-line-down-container">
+              <div class="ms-line-down"></div>
+            </div>
+            <div class="ms-line-point"></div>
+          </div>
+        </div>
+      </label>
+    </div>
+  );
+};
+
+const DownloadResume = () => {
+  const [checked, setchecked] = useState(null);
+  const props = { checked, setchecked, downloadResumeConfig };
+
+  return (
+    <div id="resume" style={{ margin: "100px", position: "relative" }}>
       <Typewriter text="Here is my Resume..." />
-      <a href={fileLink} download={fileName}>
+      {/* <a href={fileLink} download={fileName}>
         <div className="downloadBtn">
           <AttachFileIcon />
           Download Now
         </div>
-      </a>
+      </a> */}
+      <div style={{ position: "relative", margin: 100, paddingBottom: 280 }}>
+        <Download {...props} />
+      </div>
     </div>
   );
 };
